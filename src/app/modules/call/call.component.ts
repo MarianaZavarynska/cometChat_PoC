0922e971c89b6eae2c1631e4202e5167c3b84ab9
MarianaZavarunska <mariana.zavarynska@gmail.com>
2024-02-13 17:32:00 +0200

    refactored audio call listener

--- src/app/modules/call/call.component.ts
+++ src/app/modules/call/call.component.ts
@@ -1,6 +1,7 @@
 import {Component, NgZone, OnInit} from '@angular/core';
 import {CometChat} from "@cometchat-pro/chat";
 import {ActivatedRoute, Router} from "@angular/router";
+import Call = CometChat.Call;
 
 
 @Component({
@@ -38,7 +39,7 @@ export class CallComponent  implements OnInit {
   ]
   status: string | null  = null;
   incomingCall: CometChat.Call | null = null;
-  outgoingCall: CometChat.Call | null=null;
+  outgoingCall: CometChat.Call | null = null;
   error: string = '';
   loggedinUser: any = undefined;
   isSomeoneCalling: boolean = false;
@@ -53,90 +54,90 @@ export class CallComponent  implements OnInit {
     });
 
     let listnerID: string = this.loggedinUser.uid;
-    CometChat.addCallListener(
-      listnerID,
-      new CometChat.CallListener({
-        onIncomingCallReceived: (call: CometChat.Call) => {
-
-          this.zone.run(() => {
-            console.log("Incoming call:", call);
-            this.isSomeoneCalling = true;
-            this.incomingCall = call;
-            this.error = "Incoming call"
-            console.log(this.isSomeoneCalling, this.incomingCall,this.error );
-          });
-        },
-        onOutgoingCallAccepted: (call: CometChat.Call) => {
-          console.log("Outgoing call accepted:", call);
-          const sessionID = call.getSessionId();
-
-          const callSettingsCustom = new CometChat.CallSettingsBuilder()
-            .setSessionID(sessionID)
-            .enableDefaultLayout(true)
-            .forceLegacyUI(true)
-
-            // .setCallListener(
-            //   new CometChatCalls.OngoingCallListener({
-            //     onUserListUpdated: (userList) => {
-            //       console.log("user list:", userList);
-            //     },
-            //     onCallEnded: () => {
-            //       console.log("Call ended");
-            //     },
-            //     onError: (error) => {
-            //       console.log("Error :", error);
-            //     },
-            //     onMediaDeviceListUpdated: (deviceList) => {
-            //       console.log("Device List:", deviceList);
-            //     },
-            //     onUserMuted: (event) => {
-            //       // This event will work in JS SDK v3.0.2-beta1 & later.
-            //       console.log("Listener => onUserMuted:", {
-            //         userMuted: event.muted,
-            //         userMutedBy: event.mutedBy,
-            //       });
-            //     },
-            //     onScreenShareStarted: () => {
-            //       // This event will work in JS SDK v3.0.3 & later.
-            //       console.log("Screen sharing started.");
-            //     },
-            //     onScreenShareStopped: () => {
-            //       // This event will work in JS SDK v3.0.3 & later.
-            //       console.log("Screen sharing stopped.");
-            //     },
-            //     onCallSwitchedToVideo: (event) => {
-            //       // This event will work in JS SDK v3.0.8 & later.
-            //       console.log("call switched to video:", {
-            //         sessionId: event.sessionId,
-            //         callSwitchInitiatedBy: event.initiator,
-            //         callSwitchAcceptedBy: event.responder,
-            //       });
-            //     },
-            //     onUserJoined: (user) => console.log("event => onUserJoined", user),
-            //     onUserLeft: (user) => console.log("event => onUserLeft", user),
-            //   })
-            // )
-            .build();
-
-          const htmlElement = document.getElementById("my-call");
-          CometChat.startCall(callSettingsCustom, htmlElement as any)
-          this.zone.run(() => {
-            this.isCallInProgress = true;
-          });
-        },
-        onOutgoingCallRejected: (call: CometChat.Call) => {
-          console.log("Outgoing call rejected:", call);
-        },
-        onIncomingCallCancelled: (call: CometChat.Call) => {
-          console.log("Incoming call calcelled:", call);
-          const sessionID = call.getSessionId();
-          this.rejectCall(sessionID);
-        },
-        onCallEndedMessageReceived: (call: CometChat.Call) => {
-          console.log("CallEnded Message:", call);
-        }
-      })
-    );
+    // CometChat.addCallListener(
+    //   listnerID,
+    //   new CometChat.CallListener({
+    //     onIncomingCallReceived: (call: CometChat.Call) => {
+    //       //   console.log(this.isSomeoneCalling, this.incomingCall,this.error );
+    //       // this.zone.run(() => {
+    //       //   console.log("Incoming call:", call);
+    //       //   this.isSomeoneCalling = true;
+    //       //   this.incomingCall = call;
+    //       //   this.error = "Incoming call"
+    //       //   console.log(this.isSomeoneCalling, this.incomingCall,this.error );
+    //       // });
+    //     },
+    //
+    //     onOutgoingCallAccepted: (call: CometChat.Call) => {
+    //       // console.log("Outgoing call accepted:", call);
+    //       // const sessionID = call.getSessionId();
+    //       //
+    //       // const callSettingsCustom = new CometChat.CallSettingsBuilder()
+    //       //   .setSessionID(sessionID)
+    //       //   .enableDefaultLayout(true)
+    //       //   .forceLegacyUI(true)
+    //       //   // .setCallListener(
+    //       //   //   new CometChatCalls.OngoingCallListener({
+    //       //   //     onUserListUpdated: (userList) => {
+    //       //   //       console.log("user list:", userList);
+    //       //   //     },
+    //       //   //     onCallEnded: () => {
+    //       //   //       console.log("Call ended");
+    //       //   //     },
+    //       //   //     onError: (error) => {
+    //       //   //       console.log("Error :", error);
+    //       //   //     },
+    //       //   //     onMediaDeviceListUpdated: (deviceList) => {
+    //       //   //       console.log("Device List:", deviceList);
+    //       //   //     },
+    //       //   //     onUserMuted: (event) => {
+    //       //   //       // This event will work in JS SDK v3.0.2-beta1 & later.
+    //       //   //       console.log("Listener => onUserMuted:", {
+    //       //   //         userMuted: event.muted,
+    //       //   //         userMutedBy: event.mutedBy,
+    //       //   //       });
+    //       //   //     },
+    //       //   //     onScreenShareStarted: () => {
+    //       //   //       // This event will work in JS SDK v3.0.3 & later.
+    //       //   //       console.log("Screen sharing started.");
+    //       //   //     },
+    //       //   //     onScreenShareStopped: () => {
+    //       //   //       // This event will work in JS SDK v3.0.3 & later.
+    //       //   //       console.log("Screen sharing stopped.");
+    //       //   //     },
+    //       //   //     onCallSwitchedToVideo: (event) => {
+    //       //   //       // This event will work in JS SDK v3.0.8 & later.
+    //       //   //       console.log("call switched to video:", {
+    //       //   //         sessionId: event.sessionId,
+    //       //   //         callSwitchInitiatedBy: event.initiator,
+    //       //   //         callSwitchAcceptedBy: event.responder,
+    //       //   //       });
+    //       //   //     },
+    //       //   //     onUserJoined: (user) => console.log("event => onUserJoined", user),
+    //       //   //     onUserLeft: (user) => console.log("event => onUserLeft", user),
+    //       //   //   })
+    //       //   // )
+    //       //   .build();
+    //       //
+    //       // const htmlElement = document.getElementById("my-call");
+    //       // CometChat.startCall(callSettingsCustom, htmlElement as any)
+    //       // this.zone.run(() => {
+    //       //   this.isCallInProgress = true;
+    //       // });
+    //     },
+    //     onOutgoingCallRejected: (call: CometChat.Call) => {
+    //       console.log("Outgoing call rejected:", call);
+    //     },
+    //     onIncomingCallCancelled: (call: CometChat.Call) => {
+    //       console.log("Incoming call calcelled:", call);
+    //       const sessionID = call.getSessionId();
+    //       this.rejectCall(sessionID);
+    //     },
+    //     onCallEndedMessageReceived: (call: CometChat.Call) => {
+    //       console.log("CallEnded Message:", call);
+    //     }
+    //   })
+    // );
   }
 
 
@@ -181,11 +182,47 @@ export class CallComponent  implements OnInit {
   }
 
   startAudioCall(receiverUID: string) {
+    debugger
     const call = new CometChat.Call(receiverUID, CometChat.CALL_TYPE.AUDIO, CometChat.RECEIVER_TYPE.USER) as CometChat.Call;
     console.log('calling...', call);
-    CometChat.initiateCall(call).then( (outgoingCall) => {
-        console.log('Call initiated successfully:', outgoingCall);
-        this.outgoingCall = call;
+
+    CometChat.initiateCall(call).then( (callOne:any) => {
+      debugger
+      this.outgoingCall = callOne;
+
+      CometChat.addCallListener(this.loggedinUser.uid,
+        new CometChat.CallListener({
+          onOutgoingCallAccepted: (call: CometChat.Call) => {
+            console.log("Outgoing call accepted:", call);
+            const sessionID = call.getSessionId();
+
+            const callSettingsCustom = new CometChat.CallSettingsBuilder()
+              .setSessionID(sessionID)
+              .enableDefaultLayout(true)
+              .forceLegacyUI(true)
+              .build();
+
+            const ongoingCallListener = new CometChat.OngoingCallListener({
+              onCallEnded: (call: CometChat.Call) => {
+                console.log("Call ended:", call);
+                CometChat.endCall(call.getSessionId()).then((call)=> {
+                  debugger
+                  this.outgoingCall = call;
+                  console.log('======endCall=======', call)
+                  console.log('======outGoing=======', this.outgoingCall)
+                })
+              },
+
+            });
+
+            const htmlElement = document.getElementById("my-call");
+            CometChat.startCall(callSettingsCustom, htmlElement as any, ongoingCallListener)
+            // this.zone.run(() => {
+            //   this.isCallInProgress = true;
+            // });
+          },
+        })
+      )
       },
         error => {
           console.log("Call initialization failed with exception:", error);
@@ -194,32 +231,34 @@ export class CallComponent  implements OnInit {
     });
   };
 
-  startVideoCall(receiverUID: string) {
-    const call = new CometChat.Call(receiverUID, CometChat.CALL_TYPE.VIDEO, CometChat.RECEIVER_TYPE.USER) as CometChat.Call;
-    console.log('video calling...', call);
 
-    CometChat.initiateCall(call).then( (outgoingCall) => {
-        console.log('Video Call initiated successfully:', outgoingCall);
-        this.outgoingCall = call;
-      },
-      error => {
-        console.log("Call initialization failed with exception:", error);
-      }).catch(  (error) => {
-      console.log('Call initiation failed with error:', error);
-    });
+
+  startVideoCall(receiverUID: string) {
+    // const call = new CometChat.Call(receiverUID, CometChat.CALL_TYPE.VIDEO, CometChat.RECEIVER_TYPE.USER) as CometChat.Call;
+    // console.log('video calling...', call);
+    //
+    // CometChat.initiateCall(call).then( (outgoingCall) => {
+    //     console.log('Video Call initiated successfully:', outgoingCall);
+    //     this.outgoingCall = call;
+    //   },
+    //   error => {
+    //     console.log("Call initialization failed with exception:", error);
+    //   }).catch(  (error) => {
+    //   console.log('Call initiation failed with error:', error);
+    // });
   };
 
-  cancelCall(sessionID: string){
-    let status = CometChat.CALL_STATUS.CANCELLED;
-
-    CometChat.rejectCall(sessionID, status).then(
-      (call: CometChat.Call) => {
-        console.log("Call rejected successfully", call);
-        this.outgoingCall = null;
-      }).catch((error: CometChat.CometChatException) => {
-      console.log("Call rejection failed with error:", error);
-    })
-  }
+  // cancelCall(sessionID: string){
+  //   let status = CometChat.CALL_STATUS.CANCELLED;
+  //
+  //   CometChat.rejectCall(sessionID, status).then(
+  //     (call: CometChat.Call) => {
+  //       console.log("Call rejected successfully", call);
+  //       this.outgoingCall = null;
+  //     }).catch((error: CometChat.CometChatException) => {
+  //     console.log("Call rejection failed with error:", error);
+  //   })
+  // }
 
   // RECEIVE CALLS METHOOD
   acceptCall(sessionID: string){
@@ -236,13 +275,13 @@ export class CallComponent  implements OnInit {
         const htmlElement = document.getElementById("my-call");
         CometChat.startCall(callSettingsCustom as any, htmlElement as any);
 
-        this.zone.run(() => {
-          this.isCallInProgress = true;
-          this.isSomeoneCalling = true;
-          this.incomingCall = call;
-          this.error = "Accept call"
-          this.outgoingCall = call;
-        });
+        // this.zone.run(() => {
+        //   this.isCallInProgress = true;
+        //   this.isSomeoneCalling = true;
+        //   this.incomingCall = call;
+        //   this.error = "Accept call"
+        //   this.outgoingCall = call;
+        // });
       }).catch((error: CometChat.CometChatException) => {
       console.log("Call acceptance failed with error", error);
     })
@@ -255,13 +294,13 @@ export class CallComponent  implements OnInit {
       (call: CometChat.Call) => {
         console.log("Call rejected successfully", call);
 
-        this.zone.run(() => {
-          console.log("Incoming call:", call);
-          this.isSomeoneCalling = false;
-          this.incomingCall = null;
-          this.outgoingCall = null;
-          console.log(this.isSomeoneCalling, this.incomingCall,this.error );
-        });
+        // this.zone.run(() => {
+        //   console.log("Incoming call:", call);
+        //   this.isSomeoneCalling = false;
+        //   this.incomingCall = null;
+        //   this.outgoingCall = null;
+        //   console.log(this.isSomeoneCalling, this.incomingCall,this.error );
+        // });
       }).catch((error: CometChat.CometChatException) => {
       console.log("Call rejection failed with error:", error);
     })
